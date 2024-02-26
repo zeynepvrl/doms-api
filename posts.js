4663cfc29d4c44ad6c3f8a9758f0af6e92ce6891
zeynepvrl <zeynebvural450@gmail.com>
2024-02-26 19:36:54 +0300

    initial commit

---
+++ posts.js
@@ -0,0 +1,227 @@
+module.exports=[
+    {
+      "_id": "65dcbbd1093db960f86b3b7e",
+      "index": 0,
+      "guid": "194be49b-168a-41fd-9207-8d74f4992150",
+      "isActive": true,
+      "balance": "$3,422.36",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "brown",
+      "name": "Kelley Estes",
+      "gender": "female",
+      "company": "NEWCUBE",
+      "email": "kelleyestes@newcube.com",
+      "phone": "+1 (808) 590-3781",
+      "address": "239 Beverly Road, Brogan, Puerto Rico, 5637",
+      "about": "Esse amet deserunt exercitation exercitation id enim reprehenderit velit nulla labore irure. Ex dolore ad sunt voluptate commodo anim aliqua fugiat minim non cillum minim ullamco et. Dolor sunt elit occaecat duis do sunt dolore nulla pariatur. Cupidatat est ad ullamco nulla dolore ipsum est ex officia ad fugiat Lorem in. Excepteur consectetur commodo voluptate veniam consequat.\r\n",
+      "registered": "2023-03-17T08:33:38 -03:00",
+      "latitude": 5.988456,
+      "longitude": 118.751637,
+      "tags": [
+        "sint",
+        "amet",
+        "minim",
+        "reprehenderit",
+        "incididunt",
+        "ad",
+        "aliqua"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Liliana Orr"
+        },
+        {
+          "id": 1,
+          "name": "Henson Cleveland"
+        },
+        {
+          "id": 2,
+          "name": "Caroline Mckay"
+        }
+      ],
+      "greeting": "Hello, Kelley Estes! You have 4 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "65dcbbd15935f8818dfb59f0",
+      "index": 1,
+      "guid": "406cd41e-4261-4f38-8548-ecd3df0c6550",
+      "isActive": false,
+      "balance": "$2,302.26",
+      "picture": "http://placehold.it/32x32",
+      "age": 34,
+      "eyeColor": "green",
+      "name": "Snyder Harvey",
+      "gender": "male",
+      "company": "ISOSURE",
+      "email": "snyderharvey@isosure.com",
+      "phone": "+1 (873) 404-3327",
+      "address": "982 Hastings Street, Noxen, Arizona, 6581",
+      "about": "Nulla ipsum enim est culpa cupidatat. Voluptate ut ipsum laboris pariatur laborum sunt culpa aute. Cillum officia eiusmod exercitation pariatur exercitation.\r\n",
+      "registered": "2016-11-27T07:48:20 -03:00",
+      "latitude": 13.556606,
+      "longitude": -42.164768,
+      "tags": [
+        "labore",
+        "velit",
+        "aliquip",
+        "est",
+        "ad",
+        "irure",
+        "cillum"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Ollie Ballard"
+        },
+        {
+          "id": 1,
+          "name": "Bonita Dean"
+        },
+        {
+          "id": 2,
+          "name": "Dona Bishop"
+        }
+      ],
+      "greeting": "Hello, Snyder Harvey! You have 4 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "65dcbbd1501e81c24e3628a5",
+      "index": 2,
+      "guid": "36737f09-b362-4eeb-b236-77933c400eb8",
+      "isActive": false,
+      "balance": "$3,720.54",
+      "picture": "http://placehold.it/32x32",
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Sylvia Moreno",
+      "gender": "female",
+      "company": "KOG",
+      "email": "sylviamoreno@kog.com",
+      "phone": "+1 (889) 570-2390",
+      "address": "749 Madison Place, Williams, Louisiana, 2662",
+      "about": "Eu aliqua voluptate aliqua aute commodo dolor laborum ut cillum. Ipsum ex deserunt laborum enim anim exercitation. Elit commodo et non velit sint enim nisi proident velit pariatur ex. Ea dolore non cillum qui amet nulla sunt nostrud cillum. Nulla dolor aliqua quis eiusmod aliqua anim. Lorem irure eu officia minim ut qui. Nulla commodo proident laborum ut est enim cupidatat et id consectetur reprehenderit ex.\r\n",
+      "registered": "2016-05-31T01:25:06 -03:00",
+      "latitude": 71.316992,
+      "longitude": 53.896973,
+      "tags": [
+        "velit",
+        "voluptate",
+        "et",
+        "occaecat",
+        "reprehenderit",
+        "labore",
+        "consequat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Bridgett Garrison"
+        },
+        {
+          "id": 1,
+          "name": "Charles Shields"
+        },
+        {
+          "id": 2,
+          "name": "Annabelle Delacruz"
+        }
+      ],
+      "greeting": "Hello, Sylvia Moreno! You have 6 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "65dcbbd1ced56fe9ba7a4b93",
+      "index": 3,
+      "guid": "c71f9ccc-c791-4dda-80e7-365f1cb1a44f",
+      "isActive": false,
+      "balance": "$2,807.47",
+      "picture": "http://placehold.it/32x32",
+      "age": 29,
+      "eyeColor": "blue",
+      "name": "Walter Carlson",
+      "gender": "male",
+      "company": "ACUMENTOR",
+      "email": "waltercarlson@acumentor.com",
+      "phone": "+1 (872) 578-2340",
+      "address": "730 Berkeley Place, Wells, Iowa, 8725",
+      "about": "Enim reprehenderit occaecat esse dolore culpa culpa incididunt dolore velit non est pariatur laborum. Sit sint officia labore labore et qui dolor Lorem laborum tempor elit tempor labore labore. Reprehenderit commodo eiusmod fugiat labore dolore deserunt nulla. Irure ea consectetur nostrud aliqua in. Ut esse id consequat aliqua pariatur non pariatur.\r\n",
+      "registered": "2018-09-19T09:15:24 -03:00",
+      "latitude": -71.362132,
+      "longitude": -160.653018,
+      "tags": [
+        "excepteur",
+        "et",
+        "nisi",
+        "velit",
+        "voluptate",
+        "laborum",
+        "duis"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Ortiz Walton"
+        },
+        {
+          "id": 1,
+          "name": "Dotson Nguyen"
+        },
+        {
+          "id": 2,
+          "name": "Gillespie Price"
+        }
+      ],
+      "greeting": "Hello, Walter Carlson! You have 1 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "65dcbbd19afb77909c6a1219",
+      "index": 4,
+      "guid": "3c9eb591-44a1-4713-8393-3e71d88b3472",
+      "isActive": false,
+      "balance": "$3,789.99",
+      "picture": "http://placehold.it/32x32",
+      "age": 39,
+      "eyeColor": "blue",
+      "name": "Chaney Hart",
+      "gender": "male",
+      "company": "OMATOM",
+      "email": "chaneyhart@omatom.com",
+      "phone": "+1 (895) 406-2530",
+      "address": "632 Butler Place, Wakarusa, California, 9940",
+      "about": "Lorem ad esse qui cupidatat enim tempor veniam in ipsum duis laboris duis. Exercitation veniam ea reprehenderit magna in. Elit ad id sint eiusmod est.\r\n",
+      "registered": "2018-12-20T06:43:48 -03:00",
+      "latitude": -19.708138,
+      "longitude": -25.449167,
+      "tags": [
+        "veniam",
+        "aute",
+        "reprehenderit",
+        "reprehenderit",
+        "sint",
+        "mollit",
+        "adipisicing"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Meyers Torres"
+        },
+        {
+          "id": 1,
+          "name": "Kelley Barrett"
+        },
+        {
+          "id": 2,
+          "name": "Mai Burgess"
+        }
+      ],
+      "greeting": "Hello, Chaney Hart! You have 7 unread messages.",
+      "favoriteFruit": "strawberry"
+    }
+  ]
